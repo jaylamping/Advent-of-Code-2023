1e74dc7c369d27e811ff22423d65a355ae78e87e
jaylamping <35737718+jaylamping@users.noreply.github.com>
2023-12-01 20:44:53 -0600

    warmup done

--- warmup/index.js
+++ warmup/index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-fs.readFile('input.txt', 'utf-8', (err, data) => {
+fs.readFile('input.txt', 'utf8', (err, data) => {
   const answer = data.split('').reduce((acc, cur) => {
     if (cur === '(') {
       acc++;
