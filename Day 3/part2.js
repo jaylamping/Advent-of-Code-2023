b9e87dd90b2ac98a78ef2582f9f738e2139cf11a
Joey Lamping <35737718+jaylamping@users.noreply.github.com>
2023-12-04 16:00:26 -0600

    this is gonna be a long month

--- Day 3/part2.js
+++ Day 3/part2.js
@@ -1,72 +1,56 @@
 const fs = require('fs');
 
-let currNumPos = 0;
-let currGearPos = 0;
-let partCnt = 0;
+fs.readFile('input.txt', 'utf-8', (err, data) => {
+  const input = data.split('\n').filter((n) => n);
 
-fs.readFile('input.txt', 'utf8', (err, data) => {
-  const lines = data.split('\n').map((line) => line.trim());
-  const answer = lines
-    .map((line, index) => {
-      currNumPos = 0;
-      currGearPos = 0;
+  const rows = input.length;
+  const cols = input[0].length;
 
-      const gears = line.match(/\*/g)?.map((gear) => {
-        partCnt = 0;
-        const gearPos = line.indexOf(gear, currGearPos);
-        currGearPos = gearPos + gear.length;
+  const gears = {};
 
-        const prevLine = lines[Math.max(0, index - 1)]
-          .substring(gearPos - 3, gearPos + 4)
-          .split('.')
-          .map(Number);
+  const findGears = (str, num, y, x) => {
+    x = x === -1 ? 0 : x;
+    for (let k = 0; k < str.length; k++) {
+      const char = str.charAt(k);
+      if (char === '*') {
+        const ind = `${y}-${x + k}`;
+        gears[ind] = gears[ind]
+          ? [...gears[ind], parseInt(num)]
+          : [parseInt(num)];
+      }
+    }
+  };
 
-        const currLine = line
-          .substring(gearPos - 3, gearPos + gear.length + 3)
-          .match(/[0-9]/g);
+  for (let y = 0; y < rows; y++) {
+    for (let x = 0; x < cols; x++) {
+      const n = input[y][x];
+      if (isNaN(n)) continue;
 
-        const nextLine = lines[Math.min(index + 1, lines.length - 1)]
-          .substring(gearPos - 3, gearPos + 3)
-          .match(/[0-9]/g);
+      let num = n;
+      while (++x < cols) {
+        if (Number.isInteger(parseInt(input[y][x]))) num += input[y][x];
+        else break;
+      }
 
-        console.log(prevLine, currLine, nextLine);
-        if (!prevLine && !currLine && !nextLine) {
-          return [gear, false];
-        } else {
-          return [gear, true];
-        }
-      });
+      const up = input[Math.max(0, y - 1)].substring(x - num.length - 1, x + 1);
+      const down = input[Math.min(rows - 1, y + 1)].substring(
+        x - num.length - 1,
+        x + 1
+      );
+      const left = input[y][x - num.length - 1] || '';
+      const right = input[y][x] || '';
 
-      //console.log(gears);
+      findGears(up, num, y - 1, x - num.length - 1);
+      findGears(down, num, y + 1, x - num.length - 1);
+      findGears(left, num, y, x - num.length - 1);
+      findGears(right, num, y, x);
+    }
+  }
 
-      const partNumbers = line.match(/\d+/g).map((number) => {
-        const numPos = line.indexOf(number, currNumPos);
-        currNumPos = numPos + number.length;
-
-        const prevLine = lines[Math.max(0, index - 1)]
-          .substring(numPos - 1, numPos + number.length + 1)
-          .match(/[^0-9.]/g);
-
-        const currLine = line
-          .substring(numPos - 1, numPos + number.length + 1)
-          .match(/[^.0-9]/g);
-
-        const nextLine = lines[Math.min(index + 1, lines.length - 1)]
-          .substring(numPos - 1, numPos + number.length + 1)
-          .match(/[^0-9.]/g);
-
-        if (!prevLine && !currLine && !nextLine) {
-          return [number, false];
-        } else {
-          return [number, true];
-        }
-      });
-      return partNumbers;
-    })
-    .flat()
-    .filter((number) => number[1] === true)
-    .map((number) => number[0])
-    .reduce((acc, number) => acc + parseInt(number), 0);
+  const answer = Object.values(gears)
+    .filter((i) => i.length === 2)
+    .map((j) => j[0] * j[1])
+    .reduce((a, x) => a + x, 0);
 
   console.log(answer);
 });
